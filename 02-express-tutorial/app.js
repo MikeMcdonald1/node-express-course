7006259869ef31972adbe8f2d88a3cbfce9738db
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-07-30 16:49:10 -0400

    week 3 assignment part 2 complete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,6 @@
 const express = require("express"); //imports express library
 const app = express(); // creates the app (AKA app engine) that handles everything
+const { products } = require("./data");
 
 // req => middleware => res
 
@@ -8,6 +9,34 @@ app.use(express.static("./public")); // serve files from the public folder here
 // (app.get / app.post) will go here eventually
 app.get("/api/v1/test", (req, res) => {
   res.json({ message: "It worked!" });
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (!product) {
+    return res.status(404).send("That product was not found.");
+  }
+});
+
+app.get("/api/v1/query", (req, res) => {
+  const search = req.query.search;
+  const limit = req.query.limit;
+  let result = products;
+
+  if (search) {
+    result = result.filter((p) => p.name.startsWith(search));
+  }
+
+  if (limit) {
+    result = result.slice(0, parseInt(limit));
+  }
+
+  res.json(result);
 });
 
 app.all("*", (req, res) => {
