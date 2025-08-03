978e62b475975d236f1a85faf81afa3118b5d5c9
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-08-03 15:42:38 -0400

    feat(api): complete CRUD routes for /api/v1/people with controller refactor

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { people } = require("../data");
+const {
+  addPerson,
+  getPeople,
+  getPersonById,
+  updatePerson,
+  deletePerson,
+} = require("../controllers/people.js");
 
-router.get("/", (req, res) => {
-  res.json(people);
-});
+router.get("/", getPeople);
 
-router.post("/", (req, res) => {
-  if (!req.body.name) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide a name" });
-  }
+router.post("/", addPerson);
 
-  people.push({
-    id: people.length + 1,
-    name: req.body.name,
-  });
+router.get("/:id", getPersonById);
 
-  res.status(201).json({
-    success: true,
-    name: req.body.name,
-  });
-});
+router.put("/:id", updatePerson);
+
+router.delete("/:id", deletePerson);
 
 module.exports = router;
