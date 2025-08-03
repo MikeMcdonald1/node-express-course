58c3059ac60f5eaafa746bd6dd9620d798785d9c
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-08-03 10:40:48 -0400

    week4 create logger middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,10 +4,19 @@ const { products } = require("./data");
 
 // req => middleware => res
 
-app.use(express.static("./public")); // serve files from the public folder here
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().toLocaleString();
+  console.log(`${method} ${url} at ${time}`);
+  next();
+};
 
-// (app.get / app.post) will go here eventually
-app.get("/api/v1/test", (req, res) => {
+app.use(express.static("./public")); // serve static files from the public folder here
+
+app.use(logger);
+
+app.get("/api/v1/test", logger, (req, res) => {
   res.json({ message: "It worked!" });
 });
 
@@ -42,7 +51,7 @@ app.get("/api/v1/query", (req, res) => {
 app.all("*", (req, res) => {
   res.status(404).send("Page not found"); // if none of the middleware or routes match, like if it doesn't find the files, respond with 404 here
 });
-
+console.log("Server is starting up...");
 app.listen(3000, () => {
   console.log("Server is listening on port 3000..."); //turns it "on" so it can listen and take requests from the browser
 });
